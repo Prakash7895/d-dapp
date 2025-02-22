9536be20108243df177a8610891a96f3e005fd28
Prakash7895 <prakashchoudhary0141@gmail.com>
2025-02-22 19:07:53 +0530

    soulbound nft done

---
+++ script/DeploySoulboundNft.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {SoulboundNft} from "../src/SoulboundNft.sol";
+
+contract DeploySoulboundNft is Script {
+    function run() external {
+        vm.startBroadcast();
+        SoulboundNft soulboundNft = new SoulboundNft();
+        vm.stopBroadcast();
+    }
+}
---
+++ src/SoulboundNft.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract SoulboundNft is ERC721URIStorage {
+    uint256 private s_tokenCounter;
+    mapping(address => uint256[]) private s_userToTokenIds;
+    mapping(address => uint256) private s_activeProfileNft;
+
+    constructor() ERC721("ProfileNft", "PN") {
+        s_tokenCounter = 0;
+    }
+
+    // when user click on sign up on FE, this fn is called and mints a nft for profile picture
+    function createUserProfile(string memory tokenUri) public {
+        _safeMint(msg.sender, s_tokenCounter);
+        _setTokenURI(s_tokenCounter, tokenUri);
+
+        s_userToTokenIds[msg.sender].push(s_tokenCounter);
+
+        if (s_userToTokenIds[msg.sender].length == 1) {
+            s_activeProfileNft[msg.sender] = s_tokenCounter;
+        }
+
+        s_tokenCounter++;
+    }
+
+    function tokenIdExists(uint256 tokenId) public view returns (bool) {
+        return ownerOf(tokenId) != address(0);
+    }
+
+    function changeProfileNft(uint256 tokenId) public {
+        require(tokenIdExists(tokenId), "Token Id does not exist");
+        require(ownerOf(tokenId) == msg.sender, "You don't own this NFT");
+
+        s_activeProfileNft[msg.sender] = tokenId;
+    }
+
+    function getUserNfts(address user) public view returns (uint256[] memory) {
+        return s_userToTokenIds[user];
+    }
+
+    function getUserTokenUris(
+        address user
+    ) public view returns (string[] memory) {
+        uint256[] memory tokenIds = s_userToTokenIds[user];
+        string[] memory uris = new string[](tokenIds.length);
+
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            uris[i] = tokenURI(tokenIds[i]);
+        }
+
+        return uris;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public pure override(ERC721, IERC721) {
+        revert("Not allowed to transfer profile nft..");
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory data
+    ) public pure override(ERC721, IERC721) {
+        revert("Not allowed to transfer profile nft...");
+    }
+}
